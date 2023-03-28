ae880211a86101c77142d9ce8a74babe11afdb23
luxuria23 <furkancanokal1@gmail.com>
2023-03-28 21:33:14 +0300

    java01 added

---
+++ Java01/endeks.java
@@ -0,0 +1,15 @@
+package Java01;
+import java.util.Scanner;
+public class endeks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Boyunuzu giriniz (metre cinsinden)");
+        double boy = sc.nextDouble();
+        System.out.println("Kilonuzu giriniz");
+        double kilo = sc.nextDouble();
+        double endeksin = kilo / (boy * boy);
+        System.out.println("Vücut kitle endeksiniz: " + Math.floor(endeksin));
+        sc.close();
+    }
+}
