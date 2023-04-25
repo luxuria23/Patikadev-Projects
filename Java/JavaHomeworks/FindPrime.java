4d211bdd6e8dbdb6c0f16e1ed417d01a9fc3289d
luxuria23 <furkancanokal1@gmail.com>
2023-04-25 14:56:19 +0300

    comments added

--- Java/JavaHomeworks/FindPrime.java
+++ Java/JavaHomeworks/FindPrime.java
@@ -8,15 +8,18 @@ public class FindPrime {
     public static void main(String[] args) {
         //declaring variables
         boolean isPrime = true;
-        //loop
+        //loop for [2,100) numbers
         for (int i = 2; i < 100; i++) {
+            //checkhing for any divider
             for (int j = 2; j < i; j++) {
+                //if divider found 
                 if (i % j == 0) {
                     isPrime = false;
                     break;
                 }
 
             }
+            //if divider not found it prints
             if (isPrime) {
                 System.out.print(i + " ");
             }
